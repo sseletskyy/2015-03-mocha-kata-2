c7d3225af2df2800d798707faa5a74ec54b57b7e
Serge Seletskyy <sergey@clcoudconsulting.no>
2015-03-29 14:36:07 +0300

    complete

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,8 +1,13 @@
-(function(){
+(function () {
     var fizzBuzz;
 
-    fizzBuzz = function(){
-       return 1;
+    fizzBuzz = function (input) {
+        if (input == 0) return 0;
+
+        if (input % 15 == 0) return 'FizzBuzz';
+        if (input % 3 == 0) return 'Fizz';
+        if (input % 5 == 0) return 'Buzz';
+        return input;
     }
 
     module.exports = fizzBuzz;
--- test/fizz_buzz_spec.js
+++ test/fizz_buzz_spec.js
@@ -3,10 +3,37 @@
 describe("fizzBuzz", function () {
     var fizzBuzz = require('../fizzBuzz')
 
-    describe("when input is 1", function () {
-        it("should return number", function () {
-            expect(fizzBuzz(1)).to.equal(1);
+    describe("high priority case", function () {
+        describe("when input % 3 AND input % 5", function () {
+            it("should return 'FizzBuzz'", function () {
+                expect(fizzBuzz(15)).to.equal('FizzBuzz');
+                expect(fizzBuzz(30)).to.equal('FizzBuzz');
+            });
         });
-    })
+    });
+
+    describe("when input % 3 == 0", function () {
+        it("should return 'Fizz", function () {
+            expect(fizzBuzz(3)).to.equal('Fizz');
+            expect(fizzBuzz(6)).to.equal('Fizz');
+        });
+    });
+
+    describe("when input % 5 == 0", function () {
+        it("should return 'Buzz'", function () {
+            expect(fizzBuzz(5)).to.equal('Buzz');
+            expect(fizzBuzz(10)).to.equal('Buzz');
+            expect(fizzBuzz(20)).to.equal('Buzz');
+        });
+    });
+
+
+    describe("when in all other cases", function () {
+        it("should return the same number", function () {
+            expect(fizzBuzz(0)).to.equal(0);
+            expect(fizzBuzz(1)).to.equal(1);
+            expect(fizzBuzz(7)).to.equal(7);
+        });
+    });
 
 });
